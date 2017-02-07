6539c2f896b915a673de6038bf53c174a50e0ab4
turambar <turinturumarth@gmail.com>
2017-02-07 13:28:20 -0800

    Minor bug fix: had two lines of code on same line of source file. Will definitely choke.

--- scripts/extract_subjects.py
+++ scripts/extract_subjects.py
@@ -41,7 +41,8 @@ if args.verbose:
 
 stays = add_age_to_icustays(stays)
 stays = add_inunit_mortality_to_icustays(stays)
-stays = add_inhospital_mortality_to_icustays(stays)stays = filter_icustays_on_age(stays)
+stays = add_inhospital_mortality_to_icustays(stays)
+stays = filter_icustays_on_age(stays)
 if args.verbose:
 	print 'REMOVE PATIENTS AGE < 18:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], stays.SUBJECT_ID.unique().shape[0]
 
