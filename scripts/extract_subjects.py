233df549dfe3490d0b68a8bbeb74e34322b59fbb
turambar <turinturumarth@gmail.com>
2017-02-09 23:12:24 -0800

    Create new phenotype labels.

--- scripts/extract_subjects.py
+++ scripts/extract_subjects.py
@@ -1,57 +1,64 @@
 import argparse
-import os
-import sys
-
-from pandas import DataFrame
+import csv
+import yaml
 
 from mimic3benchmark.mimic3csv import *
-
+from mimic3benchmark.preprocessing import add_hcup_ccs_2015_groups, make_phenotype_label_matrix
 
 parser = argparse.ArgumentParser(description='Extract per-subject data from MIMIC-III CSV files.')
 parser.add_argument('mimic3_path', type=str, help='Directory containing MIMIC-III CSV files.')
 parser.add_argument('output_path', type=unicode, help='Directory where per-subject data should be written.')
-parser.add_argument('--event_tables', '-e', type=unicode, nargs='+', help='Tables from which to read events.', default=[ 'CHARTEVENTS', 'LABEVENTS', 'OUTPUTEVENTS' ])
+parser.add_argument('--event_tables', '-e', type=unicode, nargs='+', help='Tables from which to read events.',
+                    default=['CHARTEVENTS', 'LABEVENTS', 'OUTPUTEVENTS'])
+parser.add_argument('--phenotype_definitions', '-p', type=unicode, help='YAML file with phenotype definitions.')
 parser.add_argument('--itemids_file', '-i', type=unicode, help='CSV containing list of ITEMIDs to keep.')
 parser.add_argument('--verbose', '-v', type=int, help='Level of verbosity in output.', default=1)
 parser.add_argument('--test', action='store_true', help='TEST MODE: process only 1000 subjects, 1000000 events.')
 args, _ = parser.parse_known_args()
 
 try:
-	os.makedirs(args.output_path)
+    os.makedirs(args.output_path)
 except:
-	pass
+    pass
 
 patients = read_patients_table(args.mimic3_path)
 admits = read_admissions_table(args.mimic3_path)
 stays = read_icustays_table(args.mimic3_path)
 if args.verbose:
-	print 'START:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], stays.SUBJECT_ID.unique().shape[0]
+    print 'START:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], \
+        stays.SUBJECT_ID.unique().shape[0]
 
 print stays.ICUSTAY_ID.dtype
 
 stays = remove_icustays_with_transfers(stays)
 if args.verbose:
-	print 'REMOVE ICU TRANSFERS:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], stays.SUBJECT_ID.unique().shape[0]
+    print 'REMOVE ICU TRANSFERS:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], \
+        stays.SUBJECT_ID.unique().shape[0]
 
 stays = merge_on_subject_admission(stays, admits)
 stays = merge_on_subject(stays, patients)
 stays = filter_admissions_on_nb_icustays(stays)
 if args.verbose:
-	print 'REMOVE MULTIPLE STAYS PER ADMIT:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], stays.SUBJECT_ID.unique().shape[0]
+    print 'REMOVE MULTIPLE STAYS PER ADMIT:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], \
+        stays.SUBJECT_ID.unique().shape[0]
 
 stays = add_age_to_icustays(stays)
 stays = add_inunit_mortality_to_icustays(stays)
 stays = add_inhospital_mortality_to_icustays(stays)
 stays = filter_icustays_on_age(stays)
 if args.verbose:
-	print 'REMOVE PATIENTS AGE < 18:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], stays.SUBJECT_ID.unique().shape[0]
+    print 'REMOVE PATIENTS AGE < 18:', stays.ICUSTAY_ID.unique().shape[0], stays.HADM_ID.unique().shape[0], \
+        stays.SUBJECT_ID.unique().shape[0]
 
 stays.to_csv(os.path.join(args.output_path, 'all_stays.csv'), index=False)
 diagnoses = read_icd_diagnoses_table(args.mimic3_path)
 diagnoses = filter_diagnoses_on_stays(diagnoses, stays)
 diagnoses.to_csv(os.path.join(args.output_path, 'all_diagnoses.csv'), index=False)
 count_icd_codes(diagnoses, output_path=os.path.join(args.output_path, 'diagnosis_counts.csv'))
-print 'READY!'
+
+phenotypes = add_hcup_ccs_2015_groups(diagnoses, yaml.load(open(args.phenotype_definitions, 'r')))
+make_phenotype_label_matrix(phenotypes, stays).to_csv(os.path.join(args.output_path, 'phenotype_labels.csv'),
+                                                      index=False, quoting=csv.QUOTE_NONNUMERIC)
 
 if args.test:
     pat_idx = np.random.choice(patients.shape[0], size=1000)
@@ -61,7 +68,9 @@ if args.test:
 
 subjects = stays.SUBJECT_ID.unique()
 break_up_stays_by_subject(stays, args.output_path, subjects=subjects, verbose=args.verbose)
-break_up_diagnoses_by_subject(diagnoses, args.output_path, subjects=subjects, verbose=args.verbose)
-items_to_keep = set([ int(itemid) for itemid in DataFrame.from_csv(args.itemids_file)['ITEMID'].unique() ]) if args.itemids_file else None
+break_up_diagnoses_by_subject(phenotypes, args.output_path, subjects=subjects, verbose=args.verbose)
+items_to_keep = set(
+    [int(itemid) for itemid in DataFrame.from_csv(args.itemids_file)['ITEMID'].unique()]) if args.itemids_file else None
 for table in args.event_tables:
-	read_events_table_and_break_up_by_subject(args.mimic3_path, table, args.output_path, items_to_keep=items_to_keep, subjects_to_keep=subjects, verbose=args.verbose)
+    read_events_table_and_break_up_by_subject(args.mimic3_path, table, args.output_path, items_to_keep=items_to_keep,
+                                              subjects_to_keep=subjects, verbose=args.verbose)
