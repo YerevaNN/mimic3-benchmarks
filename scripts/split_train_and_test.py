4034844b505f20da652a417e605d391eae8d6c0a
Hrayr Harutyunyan <harhro@gmail.com>
2017-03-17 21:23:16 +0400

    Fix train and test split

--- scripts/split_train_and_test.py
+++ scripts/split_train_and_test.py
@@ -1,14 +1,18 @@
 import os
 import shutil
 import argparse
-import random
-random.seed(47297)
 
 
 parser = argparse.ArgumentParser(description='Split data into train and test sets.')
 parser.add_argument('subjects_root_path', type=str, help='Directory containing subject sub-directories.')
 args, _ = parser.parse_known_args()
 
+testset = set()
+with open("resources/testset.csv", "r") as test_set_file:
+    for line in test_set_file:
+        x, y = line.split(',')
+        if int(y) == 1:
+            testset.add(x)
 
 def move_to_partition(patients, partition):
     if not os.path.exists(os.path.join(args.subjects_root_path, partition)):
@@ -21,11 +25,9 @@ def move_to_partition(patients, partition):
 
 folders = os.listdir(args.subjects_root_path)
 folders = filter(str.isdigit, folders)
-random.shuffle(folders)
-train_cnt = int(0.85 * len(folders))
+train_patients = [x for x in folders if not x in testset]
+test_patients = [x for x in folders if x in testset]
 
-train_patients = sorted(folders[:train_cnt])
-test_patients = sorted(folders[train_cnt:])
 assert len(set(train_patients) & set(test_patients)) == 0
 
 move_to_partition(train_patients, "train")
