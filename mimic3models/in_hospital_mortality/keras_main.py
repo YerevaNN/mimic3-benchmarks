1a54a4ecf456b1a9d474798431d2b6a132b00483
Hrayr Harutyunyan <harhro@gmail.com>
2017-07-22 03:07:25 +0400

    Keep common args in one place

--- mimic3models/in_hospital_mortality/keras_main.py
+++ mimic3models/in_hospital_mortality/keras_main.py
@@ -11,41 +11,12 @@ from mimic3benchmark.readers import InHospitalMortalityReader
 from mimic3models.preprocessing import Discretizer, Normalizer
 from mimic3models import metrics
 from mimic3models import keras_utils
+from mimic3models import common_utils
 
 from keras.callbacks import ModelCheckpoint
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--network', type=str, required=True)
-parser.add_argument('--dim', type=int, default=256,
-                    help='number of hidden units')
-parser.add_argument('--depth', type=int, default=0,
-                    help='number of bi-LSTMs')
-parser.add_argument('--epochs', type=int, default=100,
-                    help='number of chunks to train')
-parser.add_argument('--load_state', type=str, default="",
-                    help='state file path')
-parser.add_argument('--mode', type=str, default="train",
-                    help='mode: train or test')
-parser.add_argument('--batch_size', type=int, default=64)
-parser.add_argument('--l2', type=float, default=0, help='L2 regularization')
-parser.add_argument('--l1', type=float, default=0, help='L1 regularization')
-parser.add_argument('--save_every', type=int, default=1,
-                    help='save state every x epoch')
-parser.add_argument('--prefix', type=str, default="",
-                    help='optional prefix of network name')
-parser.add_argument('--dropout', type=float, default=0.0, help='dropout rate')
-parser.add_argument('--batch_norm', type=bool, default=False,
-                    help='batch normalization')
-parser.add_argument('--timestep', type=float, default=0.8,
-                    help="fixed timestep used in the dataset")
-parser.add_argument('--imputation', type=str, default='previous')
-parser.add_argument('--small_part', dest='small_part', action='store_true')
-parser.add_argument('--whole_data', dest='small_part', action='store_false')
-parser.add_argument('--optimizer', type=str, default='adam')
-parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
-parser.add_argument('--beta_1', type=float, default=0.9,
-                    help='beta_1 param for Adam optimizer')
-parser.set_defaults(small_part=False)
+common_utils.add_common_arguments(parser)
 args = parser.parse_args()
 print args
 
