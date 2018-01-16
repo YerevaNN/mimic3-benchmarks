37abd8f3d0493bd8e7282685bbc237209232f694
turambar <turinturumarth@gmail.com>
2018-01-16 13:39:14 -0800

    WIP: change call in `add_age_to_icustays` to use proper view, rather than copy, syntax.

--- mimic3benchmark/mimic3csv.py
+++ mimic3benchmark/mimic3csv.py
@@ -65,7 +65,7 @@ def merge_on_subject_admission(table1, table2):
 
 def add_age_to_icustays(stays):
     stays['AGE'] = (stays.INTIME - stays.DOB).apply(lambda s: s / np.timedelta64(1, 's')) / 60./60/24/365
-    stays.AGE.ix[stays.AGE<0] = 90
+    stays.ix[stays.AGE<0,'AGE'] = 90
     return stays
 
 def add_inhospital_mortality_to_icustays(stays):
