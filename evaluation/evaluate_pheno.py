7fcf9ef1279e7f610e6bb29fc0425427422f1aa6
Hrayr Harutyunyan <harhro@gmail.com>
2018-01-31 01:22:11 +0400

    Phenotyping evaluation saves also task-specific metrics

--- evaluation/evaluate_pheno.py
+++ evaluation/evaluate_pheno.py
@@ -1,4 +1,4 @@
-from mimic3models.metrics import print_metrics_multilabel
+from mimic3models.metrics import print_metrics_multilabel, print_metrics_binary
 import sklearn.utils as sk_utils
 import numpy as np
 import pandas as pd
@@ -24,6 +24,8 @@ def main():
     df = test_df.merge(pred_df, left_on='stay', right_on='stay', how='left', suffixes=['_l', '_r'])
     assert (df['pred_1'].isnull().sum() == 0)
     assert (df['period_length_l'].equals(df['period_length_r']))
+    for i in range(1, n_tasks + 1):
+        assert (df['label_{}_l'.format(i)].equals(df['label_{}_r'.format(i)]))
 
     metrics = [('Macro ROC AUC', 'ave_auc_macro'),
                ('Micro ROC AUC', 'ave_auc_micro'),
@@ -31,8 +33,8 @@ def main():
 
     data = np.zeros((df.shape[0], 50))
     for i in range(1, n_tasks + 1):
-        data[:, i-1] = df['pred_{}'.format(i)]
-        data[:, 25 + i-1] = df['label_{}_l'.format(i)]
+        data[:, i - 1] = df['pred_{}'.format(i)]
+        data[:, 25 + i - 1] = df['label_{}_l'.format(i)]
 
     results = dict()
     results['n_iters'] = args.n_iters
@@ -42,13 +44,26 @@ def main():
         results[m]['value'] = ret[k]
         results[m]['runs'] = []
 
-    for i in range(args.n_iters):
+    for i in range(1, n_tasks + 1):
+        m = 'ROC AUC of task {}'.format(i)
+        results[m] = dict()
+        results[m]['value'] = print_metrics_binary(data[:, 25 + i - 1], data[:, i - 1], verbose=0)['auroc']
+        results[m]['runs'] = []
+
+    for iteration in range(args.n_iters):
         cur_data = sk_utils.resample(data, n_samples=len(data))
         ret = print_metrics_multilabel(cur_data[:, 25:], cur_data[:, :25], verbose=0)
         for (m, k) in metrics:
             results[m]['runs'].append(ret[k])
+        for i in range(1, n_tasks + 1):
+            m = 'ROC AUC of task {}'.format(i)
+            cur_auc = print_metrics_binary(cur_data[:, 25 + i - 1], cur_data[:, i - 1], verbose=0)['auroc']
+            results[m]['runs'].append(cur_auc)
 
-    for (m, k) in metrics:
+    reported_metrics = [m for m, k in metrics]
+    reported_metrics += ['ROC AUC of task {}'.format(i) for i in range(1, n_tasks + 1)]
+
+    for m in reported_metrics:
         runs = results[m]['runs']
         results[m]['mean'] = np.mean(runs)
         results[m]['median'] = np.median(runs)
@@ -57,11 +72,16 @@ def main():
         results[m]['2.5% percentile'] = np.percentile(runs, 97.5)
         del results[m]['runs']
 
-    print "Saving the results in {} ...".format(args.save_file)
+    print "Saving the results (including task specific metrics) in {} ...".format(args.save_file)
     with open(args.save_file, 'w') as f:
         json.dump(results, f)
 
+    print "Printing the summary of results (task specific metrics are skipped) ..."
+    for i in range(1, n_tasks + 1):
+        m = 'ROC AUC of task {}'.format(i)
+        del results[m]
     print results
+
 
 if __name__ == "__main__":
     main()
