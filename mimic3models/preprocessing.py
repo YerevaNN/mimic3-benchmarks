7e7909ab6e7814d738d770f32d19b10ce10ba57d
Hrayr Harutyunyan <harhro@gmail.com>
2018-03-29 18:19:49 +0400

    Make normalizer load/save functions works with both python 2 and 3

--- mimic3models/preprocessing.py
+++ mimic3models/preprocessing.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import numpy as np
+import platform
 import pickle
 import json
 import os
@@ -212,7 +213,10 @@ class Normalizer:
 
     def load_params(self, load_file_path):
         with open(load_file_path, "rb") as load_file:
-            dct = pickle.load(load_file, encoding='latin1')
+            if platform.python_version()[0] == '2':
+                dct = pickle.load(load_file)
+            else:
+                dct = pickle.load(load_file, encoding='latin1')
             self._means = dct['means']
             self._stds = dct['stds']
 
